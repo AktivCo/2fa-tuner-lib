f83edc58937f9deef7b6b571b27c055e1d9cde60
lo1ol <mkh199740@mail.ru>
2020-07-20 12:59:36 +0300

    add log for debain

--- implementation/debian/setup.sh
+++ implementation/debian/setup.sh
@@ -3,11 +3,13 @@
 function check_pkgs ()
 {
 	pkgs=$@
-	
+	echolog "Debian. check_pkgs pkgs: $pkgs"
 	if [[ -z "`apt-get --just-print install $pkgs | grep "NEW"`" ]]
         then
+		 echolog "new packages found"
         	return 0
         fi
+	echolog "No new packages found"
 	return 1
         
 }
@@ -19,6 +21,7 @@ function _install_packages ()
 	
 	if ! [[ -z "$check_updates" ]]
 	then
+
 		check_pkgs $pkgs
 		return $?
 	fi
@@ -30,6 +33,9 @@ function _install_packages ()
 		echoerr "Не могу установить один из пакетов: $pkgs из репозитория"
 		return 1
 	fi
+
+	echolog "new packages installed"
+        return 0
 }
 
 function _setup_local_authentication ()
@@ -37,7 +43,10 @@ function _setup_local_authentication ()
 	token=$1
 	cert_id=$2
 	user=$3
+	echolog "Debian. setup local authentication for user: $user by cert: $cert on token: $token"
+
 	home=`getent passwd $user | cut -d: -f6`
+        echolog "user home is $home"
 	
 	export_object "$token" "cert" "$cert_id" "cert.crt"
 	if [[ $? -ne 0 ]]
@@ -45,7 +54,15 @@ function _setup_local_authentication ()
 		echoerr "Не удалось экспортировать сертификат с Рутокена"
 		return 1
 	fi 
-	openssl x509 -in cert.crt -out cert.pem -inform DER -outform PEM;
+
+	out=`openssl x509 -in cert.crt -out cert.pem -inform DER -outform PEM`
+        if [[ $? -ne 0 ]]
+        then
+                echoerr "can't convert cert to DER format"
+                return 1
+        fi
+        echolog "convert cert to DER format"
+	
 	mkdir "$home/.eid" 2> /dev/null;
 	chmod 0755 "$home/.eid";
 	cat cert.pem >> "$home/.eid/authorized_certificates";
@@ -59,17 +76,20 @@ function _setup_local_authentication ()
 
 function _setup_autolock ()
 {
+	echolog "Debain. setup_autolock"
 	sudo cp "$IMPL_DIR/smartcard-screensaver.desktop" /etc/xdg/autostart/smartcard-screensaver.desktop
 	sudo systemctl daemon-reload
 }
 
 function _setup_freeipa_domain_authentication ()
 {
+	echolog "Debian update /etc/pam.d/common-auth for freeipa auth"
 	sudo sed -i -e "s/^auth.*success=2.*pam_unix.*$/auth    \[success=2 default=ignore\]    pam_sss.so forward_pass/g" -e "s/^auth.*success=1.*pam_sss.*$/auth    \[success=1 default=ignore\]    pam_unix.so nullok_secure try_first_pass/g" /etc/pam.d/common-auth
 }
 
 function _setup_ad_domain_authentication ()
 {
+	echolog "Astra update /etc/pam.d/common-auth for ad auth"
 	sudo sed -i -e "s/^auth.*success=2.*pam_unix.*$/auth    \[success=2 default=ignore\]    pam_sss.so forward_pass/g" -e "s/^auth.*success=1.*pam_sss.*$/auth    \[success=1 default=ignore\]    pam_unix.so nullok_secure try_first_pass/g" /etc/pam.d/common-auth
         return 0
 }
