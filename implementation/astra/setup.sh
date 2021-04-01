5338250325b01c69b45a34f25d1a6d04b34c8c7f
lo1ol <mkh199740@mail.ru>
2021-04-01 11:29:50 +0300

    fix pkgs for astra 1.7

--- implementation/astra/setup.sh
+++ implementation/astra/setup.sh
@@ -40,7 +40,7 @@ function install_pkgs ()
 
 function _install_packages ()
 {
-	local pkgs="libengine-pkcs11-openssl* krb5-pkinit opensc libccid pcscd libp11-2 libpam-p11 libpam-pkcs11 pcsc-tools libnss3-tools dnsutils python3-tk"
+	local pkgs="libengine-pkcs11-openssl* krb5-pkinit opensc libccid pcscd libp11-* libpam-p11 libpam-pkcs11 pcsc-tools libnss3-tools dnsutils python3-tk"
         check_update="$1"
 
         if [[ "$check_updates" ]]
